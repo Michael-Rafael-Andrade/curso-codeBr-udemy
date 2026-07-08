59c8215f8bb9fbf7ac85580bcf47c3752abac303
Michael-Rafael-Andrade <michaelrafaeldeandrade@gmail.com>
2026-07-07 21:24:58 -0300

    Create organizacao.js

---
+++ organizacao.js
@@ -0,0 +1,17 @@
+// ponto e virgula não é obrigatório no javascript
+console.log("linha de código");
+
+// código javascript é organizado em sentença de códigos
+
+// JavaScript é construído também por blocos de códigos delimitados por { }
+{
+    // ABERTURA DO BLOCO DE CÓDIGO
+    {
+        // ABERTURA DO BLOCO DE CÓDIGO DENTRO DO BLOCO DE CÓDIGO
+        console.log("Olá");
+        console.log("Mundo!"); // Padrão do curso
+        // FECHAMENTO DO BLOCO DE CÓDIGO DENTRO DO BLOCO DE CÓDIGO
+    }
+
+    // FECHAMENTO DO BLOCO DE CÓDIGO
+}
